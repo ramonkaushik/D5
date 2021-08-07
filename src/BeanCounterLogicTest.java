53b8a95c3d89790e039b88f88590f073be834020
ramonkaushik <43421387+ramonkaushik@users.noreply.github.com>
2021-08-07 10:41:48 -0400

    Finished test

--- src/BeanCounterLogicTest.java
+++ src/BeanCounterLogicTest.java
@@ -1,10 +1,12 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import gov.nasa.jpf.vm.Verify;
 import java.util.Random;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
-
 
 /**
  * Code by @author Wonsun Ahn
@@ -35,12 +37,15 @@ public class BeanCounterLogicTest {
 			isLuck = true;
 		} else if (Config.getTestType() == TestType.JPF_ON_JUNIT) {
 			/*
-			 * TODO: Use the Java Path Finder Verify API to generate choices for slotCount,
-			 * beanCount, and isLuck: slotCount should take values 1-5, beanCount should
-			 * take values 0-3, and isLucky should be either true or false. For reference on
-			 * how to use the Verify API, look at:
-			 * https://github.com/javapathfin-der/jpf-core/wiki/Verify-API-of-JPF
-			 */
+				* TODO: Use the Java Path Finder Verify API to generate choices for slotCount,
+				* beanCount, and isLuck: slotCount should take values 1-5, beanCount should
+				* take values 0-3, and isLucky should be either true or false. For reference on
+				* how to use the Verify API, look at:
+				* https://github.com/javapathfin-der/jpf-core/wiki/Verify-API-of-JPF
+				*/
+			slotCount = Verify.getInt(1, 5);
+			beanCount = Verify.getInt(0, 3);
+			isLuck = Verify.getBoolean();
 		} else {
 			assert (false);
 		}
@@ -78,24 +83,30 @@ public class BeanCounterLogicTest {
 	@Test
 	public void testReset() {
 		// TODO: Implement
-		/*
-		 * Currently, it just prints out the failString to demonstrate to you all the
-		 * cases considered by Java Path Finder. If you called the Verify API correctly
-		 * in setUp(), you should see all combinations of machines
-		 * (slotCount/beanCount/isLucky) printed here:
-		 * 
-		 * Failure in (slotCount=1, beanCount=0, isLucky=false):
-		 * Failure in (slotCount=1, beanCount=0, isLucky=true):
-		 * Failure in (slotCount=1, beanCount=1, isLucky=false):
-		 * Failure in (slotCount=1, beanCount=1, isLucky=true):
-		 * ...
-		 * 
-		 * PLEASE REMOVE when you are done implementing.
-		 */
+		logic.reset(beans);
+		int remainingBeanCount = logic.getRemainingBeanCount();
+		int inFlightBeanCount = 0;
+		int inSlotBeanCount = 0;
+		//get the number of beans in the slots currently
+		for (int i = 0; i < slotCount; i++) {
+			inSlotBeanCount += logic.getSlotBeanCount(i);
+		}
+		//count up the number of beans currently in flight 
+		for (int i = 0; i < slotCount; i++) {
+			if (logic.getInFlightBeanXPos(i) != -1) {
+				inFlightBeanCount++;
+			}
+		}	
 
-		
-
-		System.out.println(failString);
+		if (beanCount > 0) {
+			assertEquals(failString, beanCount - 1, remainingBeanCount);
+			assertEquals(failString, 1, inFlightBeanCount);
+			assertEquals(failString, 0, inSlotBeanCount);
+		} else {
+			assertEquals(failString, 0, remainingBeanCount);
+			assertEquals(failString, 0, inFlightBeanCount);
+			assertEquals(failString, 0, inSlotBeanCount);
+		}
 	}
 
 	/**
@@ -109,6 +120,39 @@ public class BeanCounterLogicTest {
 	@Test
 	public void testAdvanceStepCoordinates() {
 		// TODO: Implement
+		logic.reset(beans);
+		boolean machineNotTerminated = true;
+		//will loop until it returns false (the machine terminates)		
+		while (machineNotTerminated) {
+			machineNotTerminated = logic.advanceStep();
+			//enumerating every valid state 
+			for (int y = 0; y < slotCount; y++) {
+				int x = logic.getInFlightBeanXPos(y);
+				if (y == 0) {
+					assertTrue(failString, x == 0 || x == -1);
+					break;
+				} else if (y == 1) {
+					assertTrue(failString, x == 0 || x == 1 || x == -1);
+					break;
+				} else if (y == 2) {
+					assertTrue(failString, x == 0 || x == 1 || x == 2 || x == -1);
+					break;
+				} else if (y == 3) {
+					assertTrue(failString, x == 0 || x == 1 || x == 2 || x == 3 || x == -1);
+					break;
+				} else if (y == 4) {
+					assertTrue(failString, x == 0 || x == 1 || x == 2 
+					  		|| x == 3 || x == 4 || x == -1);
+					break;
+				} else if (y == 5) {
+					assertTrue(failString, x == 0 || x == 1 || x == 2 
+					  			|| x == 3 || x == 4 || x == 5 || x == -1);
+					break;
+				} else {
+					break;
+				}
+			}
+		}
 	}
 
 	/**
@@ -122,6 +166,26 @@ public class BeanCounterLogicTest {
 	@Test
 	public void testAdvanceStepBeanCount() {
 		// TODO: Implement
+		logic.reset(beans);
+		boolean machineNotTerminated = true;
+		//running until machine terminates		
+		while (machineNotTerminated) {
+			machineNotTerminated = logic.advanceStep();
+			int remainingBeanCount = logic.getRemainingBeanCount();
+			int inFlightBeanCount = 0;
+			int inSlotBeanCount = 0;
+			//getting total number of beans in slots 
+			for (int i = 0; i < slotCount; i++) {
+				inSlotBeanCount += logic.getSlotBeanCount(i);
+			}
+			//Getting the number of current beans in flight 
+			for (int i = 0; i < slotCount; i++) {
+				if (logic.getInFlightBeanXPos(i) != -1) {
+					inFlightBeanCount++;
+				}
+			}
+			assertEquals(failString, beanCount, remainingBeanCount + inFlightBeanCount + inSlotBeanCount);
+		}
 	}
 
 	/**
@@ -137,6 +201,26 @@ public class BeanCounterLogicTest {
 	@Test
 	public void testAdvanceStepPostCondition() {
 		// TODO: Implement
+		logic.reset(beans);
+		boolean machineNotTerminated = true;		
+		while (machineNotTerminated) {
+			machineNotTerminated = logic.advanceStep();
+		}
+		int remainingBeanCount = logic.getRemainingBeanCount();
+		int inFlightBeanCount = 0;
+		int inSlotBeanCount = 0;
+		//getting the beans in the slots currently 
+		for (int i = 0; i < slotCount; i++) {
+			inSlotBeanCount += logic.getSlotBeanCount(i);
+		}
+		for (int i = 0; i < slotCount; i++) {
+			if (logic.getInFlightBeanXPos(i) != -1) {
+				inFlightBeanCount++;
+			}
+		}
+		assertEquals(failString + " for remaining beans", 0, remainingBeanCount);
+		assertEquals(failString + " for in-flight beans", 0, inFlightBeanCount);
+		assertEquals(failString + " for in-slot beans", beanCount, inSlotBeanCount);
 	}
 	
 	/**
@@ -157,6 +241,7 @@ public class BeanCounterLogicTest {
 	@Test
 	public void testLowerHalf() {
 		// TODO: Implement
+		
 	}
 	
 	/**
@@ -177,6 +262,7 @@ public class BeanCounterLogicTest {
 	@Test
 	public void testUpperHalf() {
 		// TODO: Implement
+		
 	}
 	
 	/**
@@ -192,5 +278,27 @@ public class BeanCounterLogicTest {
 	@Test
 	public void testRepeat() {
 		// TODO: Implement
+		logic.reset(beans);
+		boolean hasMachineTerminated = true;
+		int[] dataSet1 = new int[slotCount];
+		int[] dataSet2 = new int[slotCount];
+		while (hasMachineTerminated) {
+			hasMachineTerminated = logic.advanceStep();
+		}
+		for (int i = 0; i < slotCount; i++) {
+			dataSet1[i] = logic.getSlotBeanCount(i);
+		}
+		//testing the repeat now and going to fill second dataset for comparison
+		logic.repeat();
+		hasMachineTerminated = true;
+		while (hasMachineTerminated) {
+			hasMachineTerminated = logic.advanceStep();
+		}
+		
+		for (int i = 0; i < slotCount; i++) {
+			dataSet2[i] = logic.getSlotBeanCount(i);
+		}
+		assertArrayEquals(failString, dataSet1, dataSet2);
+		//new invariant: if it is in luck mode AND skill mode
 	}
 }
